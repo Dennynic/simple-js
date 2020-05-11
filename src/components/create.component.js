2dfbd7dcdd590b9d8759fd2eca069a2d5041ab7a
Dennynic <joragdetibil@gmail.com>
2020-05-11 23:43:18 +0500

    Add form component and validators

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,29 @@
 import { Component } from "../core/component";
+import { Form } from "../core/form";
+import { Validators } from "../core/validators";
 
 export class CreateComponent extends Component {
   constructor(id) {
     super(id);
+  }
+
+  init() {
+    this.$element.addEventListener("submit", submitFormHandler.bind(this));
+    this.form = new Form(this.$element, {
+      title: [Validators.required],
+      fulltext: [Validators.required, Validators.minLength(10)],
+    });
+  }
+}
+
+function submitFormHandler(event) {
+  event.preventDefault();
+  if (this.form.isValid()) {
+    const formData = {
+      type: this.$element.type.value,
+      ...this.form.value(),
+    };
+    this.form.clear();
+    console.log("formData", formData);
   }
 }
